8c0c623d11260c8343fad7e129ac07fa42233138
Tarun Lahrod <lahrod.tarun46@gmail.com>
2019-07-12 00:54:18 +0530

    added opening animation

--- app/src/main/java/com/example/metroapp/MainActivity.java
+++ app/src/main/java/com/example/metroapp/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.metroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView logo;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = findViewById(R.id.logo);
+        textView = findViewById(R.id.text);
+
+        openingAnimation();
+    }
+
+    public void openingAnimation(){
+        logo.animate().scaleXBy(1.25f).scaleYBy(1.25f).rotation(180).setDuration(1000);
+        textView.animate().translationY(200).setDuration(1000);
     }
 }
