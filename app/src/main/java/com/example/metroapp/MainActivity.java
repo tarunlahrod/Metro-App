d3808d49517f786bd98da603d5cab38b2b7836f5
Prateek Tiwari <prateektiwari5399@gmail.com>
2019-07-28 02:11:04 +0530

    Some UI changes

--- app/src/main/java/com/example/metroapp/MainActivity.java
+++ app/src/main/java/com/example/metroapp/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openingAnimation(){
-        logo.animate().scaleXBy(0.5f).scaleYBy(0.5f).setDuration(1000);
-        textView.animate().translationY(200).setDuration(1000);
+        logo.animate().scaleXBy(0.5f).scaleYBy(0.5f).setDuration(2000);
+        textView.animate().translationY(200).setDuration(2000);
     }
 }
