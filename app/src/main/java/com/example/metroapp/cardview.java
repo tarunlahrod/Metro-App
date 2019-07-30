2fc12391a17bbfed48e65bd2a192a061b4cff536
Prateek Tiwari <prateektiwari5399@gmail.com>
2019-07-31 01:02:57 +0530

    Added some loggers to understand code

--- app/src/main/java/com/example/metroapp/cardview.java
+++ app/src/main/java/com/example/metroapp/cardview.java
@@ -18,22 +18,23 @@ public class cardview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardview);
-
+//   Initializing card1 and its attributes
         card1=findViewById(R.id.card1);
         card1_cost=findViewById(R.id.card1_cost);
         card1_image=findViewById(R.id.card1_image);
         card1_time=findViewById(R.id.card1_time);
-
+//   Initializing card2 and its attributes
         card2=findViewById(R.id.card2);
         card2_cost=findViewById(R.id.card2_cost);
         card2_image=findViewById(R.id.card2_image);
         card2_time=findViewById(R.id.card2_time);
-
+//   Initializing card3 and its attributes
         card3=findViewById(R.id.card3);
         card3_cost=findViewById(R.id.card3_cost);
         card3_image=findViewById(R.id.card3_image);
         card3_time=findViewById(R.id.card3_time);
-
+//   Setting the values
+//        card1_time.setText();
 
 
         card1.setOnClickListener(new View.OnClickListener() {
